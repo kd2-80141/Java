5352c188e92ff3c634a075e09b19fd6951653500
Nishant-Maske <nishantmaske8@gmail.com>
2023-10-17 20:13:46 +0530

    Added Assignment4

---
+++ Assignment-4/Assignment-04/src/com/app/fruits/Fruit.java
@@ -0,0 +1,90 @@
+package com.app.fruits;
+/*Q1 Apply inheritance n polymorphism
+a) Arrange Fruit,Apple,Orange,Mango in inheritance hierarchy
+b) Properties (instance variables) : color : String , weight : double , name:String, isFresh : boolean
+c) Add suitable constructors.
+d) Override toString correctly to return state of all fruits (including : name ,color , weight )
+e) Add a taste() method : public String taste()
+For Fruit : it should return "no specific taste"
+Apple : should return "sweet n sour"
+Mango : should return "sweet"
+Orange : should return "sour"
+f) Add all of above classes under the package "com.app.fruits"
+g) Create java application FruitBasket , with main method , as a tester
+h) Prompt user for the basket size n create suitable data structure and give options
+0. Exit
+1. Add Mango
+case 1 : boundary checking
+basket[counter++]=new Mango(nm,weight,color);
+break;
+2. Add Orange
+3. Add Apple
+NOTE : You will be adding a fresh fruit in the basket , in all of above options.
+4. Display names of all fruits in the basket.
+eg : for-each --- null checking --getName()
+5. Display name,color,weight , taste of all fresh fruits , in the basket.
+eg : for-each , null checking --toString , taste, isFresh : getter
+6. Display tastes of all stale(not fresh) fruits in the basket.
+7. Mark a fruit as stale
+i/p : index
+eg : setter : isFresh : false
+o/p : error message (in case of invalid index) or mark it stale
+8. Mark all sour fruits stale (optional)
+eg : for-each , taste --equals(String)*/
+public class Fruit {
+	private String name;
+	private String colour;
+	private  double weight ;
+	private boolean isFresh;
+ 	
+	public Fruit() {
+		
+		
+	}
+
+	public Fruit(String name, String colour, double weight , boolean isFresh) {
+		
+		this.name = name;
+		this.colour = colour;
+		this.weight = weight;
+		this.isFresh=isFresh;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public boolean isFresh() {
+		return isFresh;
+	}
+
+	public void setFresh(boolean isFresh) {
+		this.isFresh = isFresh;
+	}
+	
+	
+	
+	
+	
+
+}
