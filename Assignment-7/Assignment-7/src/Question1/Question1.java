03c6e8a9e0b6d7596790e0b3f18c3a9ed403e294
Nishant-Maske <nishantmaske8@gmail.com>
2023-10-17 22:44:11 +0530

    Assignment-7 done

---
+++ Assignment-7/Assignment-7/src/Question1/Question1.java
@@ -0,0 +1,33 @@
+package Question1;
+
+import java.util.Scanner;
+
+public class Question1 {
+	
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Please enter a len");
+			int len = sc.nextInt();
+			sc.nextLine();
+			String [] arr = new String[len];
+			for(int i = 0 ; i < len ; i++)
+			{
+				System.out.print("Enter a string ");
+				arr[i] = sc.nextLine();
+			}
+			
+			for(int i = 0 ; i < len ; i++)
+			{
+				for(int j = i+1 ; j < len ; j++)
+				if(arr[i].equals(arr[j]))
+				{
+					System.out.println("Duplicate : " + arr[i]);
+				}
+					
+			}
+			
+			
+		}
+
+}
